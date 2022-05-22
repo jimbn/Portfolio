dcdf3171482fcce4983445a65c0552aed847554d
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-05-21 20:09:52 -0700

    initial set up for about content

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1>About Me</h1>
+            <div>
+                <p>My name is Jim. I am an aspiring full stack developer, focusing on MERN stack.
+                </p>
+            </div>
+        </section>
+    )
+}
