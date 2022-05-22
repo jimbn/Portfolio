f00280006669eb5a3d9d009244aa43dc53e0f1f8
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-05-22 16:20:26 -0700

    add projectArray

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,9 @@ function About() {
         <section>
             <h1>About Me</h1>
             <div>
-                <p>My name is Jim. I am an aspiring full stack developer, focusing on MERN stack.
+                <p>
+                My experience in coding has not been that extensive. Although my time in coding has been short, I try to dedicate myself in improving every day. 
+                I enjoy learning different skills while honing the knowledge I currently have. I am currently working with MERN stack, improving and perfienct with using MongoleDB, Express, React and Node.
                 </p>
             </div>
         </section>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,3 +1,22 @@
 import React from "react";
 
+const projectArray = [
+    {
+        projectName: "Human's Best Friend",
+        description: "First project.",
+        language: "HTML, CSS, JS",
+        library: "MD Bootstrap",
+        site: "https://jimbn.github.io/Project-01/",
+        images: ""
+    },
+    {
+        projectName: "Only_Friends",
+        description: "Second projecct",
+        language: "HTML, CSS, JS",
+        library: "MySql2, Express, Node, Multer, Handlebars",
+        site: "https://only-friendz.herokuapp.com/",
+        images: ""
+    }
+]
+
 export default Project;
