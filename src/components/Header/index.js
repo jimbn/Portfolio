46b9acdcc0034c78a71eb65793c01f7da699cc44
jimbn <nguyenb.jim@gmail.com>
2022-06-09 12:43:57 -0700

    add transition nav button & menu transition in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,30 @@ import React from "react";
 import  './style.css';
 
 function Header (props) {
+    let menuOpen =false;
+
+    const addClass=()=>{
+        menuOpen = !menuOpen;
+
+        const menu = document.querySelector('.menu');
+
+        if(menuOpen){
+            menu.classList.add('menu-open')
+        } else {
+            menu.classList.remove('menu-open')
+        }
+    }
 
     return (
-        
+    <>    
         <header className="header-style">
             <div><h1 className="title">Jim Bui Nguyen</h1></div>
-            {props.children}
+            <div className="menu-button diamond" onClick={ addClass}>
+            </div>          
         </header>
+        <div className="menu">{props.children}</div>
+    </>
+
     )
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,6 +13,15 @@ function Nav(props) {
     document.title = capitalizeFirstLetter(currentPage.name);
   }, [currentPage]);
 
+  const removeClass=()=>{
+
+    const menu = document.querySelector('.menu');
+
+    
+    menu.classList.remove('menu-open')
+  
+}
+
   return (    
       <nav className='nav'>
         <ul className="flex-row">
@@ -24,7 +33,7 @@ function Nav(props) {
               key={Page.name}
             >
               <span
-                onClick={() => setCurrentPage(Page)}
+                onClick={() => setCurrentPage(Page) & removeClass()}
               >
                 {capitalizeFirstLetter(Page.name)}
               </span>
