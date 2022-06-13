538a9aa453703a1773b10bff014d6859fd92dd0a
jimbn <nguyenb.jim@gmail.com>
2022-06-13 12:57:34 -0700

    add description to project

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,7 +10,7 @@ function Project () {
     const array = [
         {
             name: 'Party Hearty',
-            description: 'Third Project',
+            description: 'Party Hearty is a web application that allows user to sign up and create events that contains their id key. Each event has details pertiaing to the event and map pinpointing to the exact location. Additionally, user are able to update and delete their events that they create. Along with presenting the event, the application allow the user to send out invite to other user on the platform and provide a guest list where other user can RSVP to.',
             language: "JavaScript",
             tools: ["MongodDb", "Express.js", "React", "Node.js", "GraphQL", "Leaflet",
         "Geosearch", "React-Bootstrap", "Material UI", "Heroku"],
@@ -20,7 +20,7 @@ function Project () {
         },
         {
             name: "Only_Friends",
-            description: "Second projecct",
+            description: "Only_Friends is a social media web platform where users can connect with others, create unique profiles that enables them to post about their interest and comment on other users' post. The application also allow for user to upload image when making their post and to personalize their profile with their own profile image",
             language: "JavaScript",
             tools: ["SQL", "Express.js", "Node.js", "Handlebars.js", "Multer", "Heroku"],
             gitHub:'https://github.com/MCORTEZM1/Only_Friends',
@@ -29,7 +29,7 @@ function Project () {
         },
         {
             name: "Human's Best Friend",
-            description: "First project.",
+            description: "Human's Best Friend is a web application that utalize third-party API to serch for dog related facility near the area that they specify. Additionally, the application provide user with a fun fact about various dog breed through the use of a drop down menu fetching third-party api",
             language: "JavaScript",
             tools: ["MD Bootstrap", "Third-Party APIs"],
             gitHub:'https://github.com/jimbn/Project-01',
