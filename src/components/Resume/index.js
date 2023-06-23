c8d1828be04f5f2948e3f938e70d4862ca8b3b68
Buubonic <nguyenb.jim@gmail.com>
2023-06-22 18:44:03 -0700

    add resume and fix padding

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,7 +8,7 @@ function Resume() {
 
       <div className='resume-dl'>
         <h5>
-          Download my <a href='./assets/Resume_JimBuiNguyen.pdf' download='Resume_JimBuiNguyen.pdf'>resume</a>
+          Download my <a href='./assets/Resume.pdf' download='Resume.pdf'>RESUME</a>
         </h5>
       </div>
       <div>
@@ -16,39 +16,31 @@ function Resume() {
           <h3>Experience</h3>
           <ul className='experience'>
             <li>
-              <h4> University of California, Berkeley, Coding Bootcamp - <span>Full Stack Developer Certification</span></h4>
-              <p> December 2021 - June 2022 (Completed)</p>
+              <h4>University of California, Berkeley, Coding Bootcamp - <span>Full Stack Developer Certification</span></h4>
+              <p>June 2022 (Completed)</p>
             </li>
           </ul>
         </div>
       </div>
 
-      <div className="resume-flex">
-        <div className="resume-background">
-          <h3>Front-end Proficiencies</h3>
-          <ul className="resume-proficiencies">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>jQuery</li>
-            <li>responsive design</li>
-            <li>React</li>
-            <li>Bootstrap</li>
-          </ul>
-        </div>
-
-        <div className="resume-background">
-          <h3>Back-end Proficiencies</h3>
-          <ul className="resume-proficiencies">
-            <li>APIs</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>REST</li>
-            <li>GraphQL</li>
-          </ul>
-        </div>
+      <div className="resume-background">
+        <h3>Skills</h3>
+        <ul className="resume-proficiencies">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Mobile Responsive Design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
       </div>
     </section>
   )
